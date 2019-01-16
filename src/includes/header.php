6932051f3cf30bff497d0d335928c5a65c553845
karabenemsi <florian@florianlubitz.de>
2019-01-16 17:34:19 +0100

    menu header and database

--- src/includes/header.php
+++ src/includes/header.php
@@ -33,7 +33,7 @@
           <li><a href="./menu.php">Menus</a></li>
         </ul>
         <ul>
-          <li><a href="">Download Menu</a></li>
+          <li><a href="./menu.pdf" target="_blank">Download Menu</a></li>
         </ul>
         <ul>
           <li><a href="./locations.php">Locations</a></li>
