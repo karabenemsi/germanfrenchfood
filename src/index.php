ee9ac2e3e0ce0089a47cc7c3f00083221d904449
karabenemsi <florian@florianlubitz.de>
2019-01-16 14:31:07 +0100

    Revert "Revert "Stuff""
    This reverts commit fa92588d98fb6af582b3c92fb39ed198029020dd.

--- src/index.php
+++ src/index.php
@@ -12,13 +12,30 @@
 
     </section>
 
+        <section class="split-text">
+        
+      <div class="half">
+        <div class="container">
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic iure debitis est soluta itaque ratione rerum, aperiam modi, exercitationem iste quo voluptatem illo necessitatibus error accusamus odit. Similique eveniet explicabo id debitis. Possimus nisi odit, dolores quae saepe ullam impedit.</p>
+        </div>
+      </div>
+      <div class="half">
+        <div class="container">
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <p>Lorem ipsumum dolor sit amet consectetur adipisicing elit. Aliquam blanditiis est odio sequi vitae consequatur sit delectus temporibus! Consequuntur placeat deserunt, qui eum ab amet aspernatur sapiente perspiciatis impedit a ullam maxime architecto illo ratione totam recusandae itaque veritatis veniam!</p>
+        </div>
+      </div>
+
+    </section>
+
     
 
     <section class="homemenu">
 
         <div class="homebox">
             <h1> Great Headline </h1>
-            
+
         </div>
 
         <a href="" class="homebox">
@@ -46,25 +63,14 @@
         <h1> Great Headline </h1>
 
         </div>
-    <section class="split-text">
-        
-      <div class="half">
-        <div class="container">
-          <h2>Lorem ipsum dolor sit amet.</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic iure debitis est soluta itaque ratione rerum, aperiam modi, exercitationem iste quo voluptatem illo necessitatibus error accusamus odit. Similique eveniet explicabo id debitis. Possimus nisi odit, dolores quae saepe ullam impedit.</p>
-        </div>
-      </div>
-      <div class="half">
-        <div class="container">
-          <h2>Lorem ipsum dolor sit amet.</h2>
-          <p>Lorem ipsumum dolor sit amet consectetur adipisicing elit. Aliquam blanditiis est odio sequi vitae consequatur sit delectus temporibus! Consequuntur placeat deserunt, qui eum ab amet aspernatur sapiente perspiciatis impedit a ullam maxime architecto illo ratione totam recusandae itaque veritatis veniam!</p>
-        </div>
-      </div>
+  </section>
 
-    </section>
 
-    <div style="height:2000px"></div>
-
+        <section class="video" style="background-image:linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.6)), url(./images/hero1.jpg);">
+          <h2>Freshly Made</h2>
+          <p>Our ingredients are grown regionally and selected by hand by our chefs. The dishes are freshly prepared for you after your order and then served at your table. In order to provide you with a high quality diet, we constantly carry out quality controls and support sustainable agriculture.</p>
+        </section>
+    
     </main>
 <?php
 
---
+++ src/men.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>French menu</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<main>
+		<section class="titre" style="background-image: url(images/region/ile_vierge.jpg);">>
+			<h1> French menu</h1>
+		</section>
+		<section>
+			<div class="menu-item" style="background-image: url(images/region/dune_pillat.jpg);">
+				<a href="">Breton Menu</a>
+			</div>
+			<div class="menu-item alsace" style="background-image: url(images/region/place_stanislas.jpg);">>
+
+				<a href="">Alsace - Lorraine Menu</a>
+			</div>
+			<div class="menu-item" style="background-image: url(images/region/hotel_dieu.jpg);">>
+				<a href="">Lyonnais Menu</a>
+			</div>
+
+			<div class="menu-item" style="background-image: url(images/region/pont_notre_dame.jpg);">>
+				<a href="">Aquitaine Menu</a>
+
+				
+			</div>
+			<div class="menu-item" style="background-image: url(images/region/aiguille_du_midi.jpg);">>
+				<a href="">Parisian Menu</a>
+			
+			</div>
+			<div class="menu-item" style="background-image: url(images/region/ile_vierge.jpg);">>
+				<a href="">Savoyard Menu</a>
+				
+			</div>
+			
+
+		</section>
+	</main>
+</body>
+</html>
