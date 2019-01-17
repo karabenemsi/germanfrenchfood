8d85b41bc83eee59d08f2543c313b78099a6b4f2
Nathan NOUZILLE <nouzilln@iut-acy.local>
2019-01-17 10:42:46 +0100

    sohne scheisse

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -2,7 +2,8 @@
 
 
 	<article id = alignment>
-		<p><h2>2003,</h2> Six friends,<p id="GerColor"> German</p> and <p id="FrColor">French</p> created GermanFrenchFood. They wanted to gives the possibility to everyone to take advantage of the food of their countries. GermanFrenchFood have the goal to permit to all people to discover all the specialties. GFF is happy to serve you their best recipes.</p>
+		<p><h2>2003,</h2> Six friends,<p id="GerColor"> German</p> and <p id="FrColor">French</p> created GermanFrenchFood. They wanted to gives the possibility to everyone to take advantage of the food of their countries. GermanFrenchFood have the goal to permit to all people to discover all the specialties. GFF is happy to serve you their best recipes.
+	haha.</p>
 	</article>
 <? /* Small read more button to have more detail on the company, with link to a detailed page*/
 ?>
